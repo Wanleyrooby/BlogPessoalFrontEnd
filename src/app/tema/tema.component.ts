6a0dfd3266d210f91a60610986cb1c77d9e525aa
Wanleyrooby <alexroob96@gmail.com>
2022-07-08 17:54:14 -0300

    crud Tema completo

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -9,7 +11,11 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class TemaComponent implements OnInit {
 
+  tema: Tema = new Tema()
+  listaTema: Tema[]
+
   constructor(
+    private temaService: TemaService,
     private router: Router
   ) { }
 
@@ -17,7 +23,27 @@ export class TemaComponent implements OnInit {
     if(environment.token == ''){
      
       this.router.navigate(['/login'])
+    }
+
+    this,this.findAllTemas()
   }
 
-}
+  findAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[])=>{
+      this.listaTema = resp
+    })
+
+  }
+
+  cadastrarTema(){
+    this.temaService.postTema(this.tema).subscribe((resp: Tema)=>{
+      this.tema = resp
+      alert('Tema cadastrado com sucesso')
+      this.findAllTemas()
+
+      this.tema = new Tema()
+    })
+  }
+  
+
 }
